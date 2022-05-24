0a77d2b4afdb7c7c87b77cf02f4b2c7fd1687caf
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2022-05-24 22:24:00 +0200

    Added number of cars for the trains, Added name for the stations

--- database/migrations/2022_05_23_214257_create_stations_table.php
+++ database/migrations/2022_05_23_214257_create_stations_table.php
@@ -15,6 +15,7 @@ class CreateStationsTable extends Migration
     {
         Schema::create('stations', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->string('city');
             $table->unsignedBigInteger('admin')->nullable();
             $table->foreign('admin')->references('id')->on('admins')->onDelete('set null');
