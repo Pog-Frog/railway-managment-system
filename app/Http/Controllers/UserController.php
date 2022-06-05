8884a10822b43d7b5915b0cb3a0200e1f52d8fea
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2022-06-05 18:31:20 +0200

    User: updated user files

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -192,6 +192,8 @@ class UserController extends Controller
         $booked_ticket->stops_station = $stops_station->id;
         $booked_ticket->seat = $seat->id;
         $booked_ticket->save();
+        $seat->seat_availability = "false";
+        $seat->save();
         $pdf = PDF::loadView('user/ticket', compact('booked_ticket', 'seat'));
         return $pdf->download('Ticket.pdf');
     }
